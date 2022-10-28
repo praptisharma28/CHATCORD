53f500edd85b54dd20c31a907a3c90c6690d68b5
srivastavamuskan <muskansrivastava262@gmail.com>
2022-10-28 21:36:08 +0530

    added js file

---
+++ _html_css/js/main.js
@@ -0,0 +1,20 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
